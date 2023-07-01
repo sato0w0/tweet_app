d774622b84955514c11d5db8393031bf0a4d9514
sato0w0 <130539468+sato0w0@users.noreply.github.com>
2023-07-01 12:57:11 +0000

    更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/index'
+  get 'posts/:id' => 'posts#show'
   get '/' => 'home#top'
   get 'about' => 'home#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
