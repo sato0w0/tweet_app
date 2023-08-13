89116ffb809322e06f4e4ebf449cf277e75deb02
sato0w0 <130539468+sato0w0@users.noreply.github.com>
2023-08-13 15:13:56 +0000

    更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get "signup" => "users#new"
+  post "users/create" => "users#create"
+  get "users/:id/edit" => "users#edit"
+  post "users/:id/update" => "users#update"
+
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+  
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
